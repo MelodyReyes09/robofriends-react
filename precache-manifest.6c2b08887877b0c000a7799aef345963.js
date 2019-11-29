54f81bdd7c661864cb36c9f3ab66cd300a794853
melody <melodyblossom09@gmail.com>
2019-11-29 19:16:47 +0800

    Updates

---
+++ precache-manifest.6c2b08887877b0c000a7799aef345963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8168f48e1e2a531f7c2f33f95d53679e",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a98317eca1936abd7343",
+    "url": "/robofriends-react/static/css/main.f8179c0e.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends-react/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "a98317eca1936abd7343",
+    "url": "/robofriends-react/static/js/main.44ed4fcf.chunk.js"
+  },
+  {
+    "revision": "433f736b572bf9e5d91f",
+    "url": "/robofriends-react/static/js/runtime-main.6be1e674.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
